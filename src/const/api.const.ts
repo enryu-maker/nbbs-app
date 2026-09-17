1eb671296b61d88ba10652c7e1330e4459e13fc5
enryu-maker <akifkhan60067@gmail.com>
2026-09-17 17:05:22 +0530

    fix: update CONTACT_US_ENDPOINT to remove trailing slash for consistency

--- src/const/api.const.ts
+++ src/const/api.const.ts
@@ -4,6 +4,6 @@ export const CREDIBILITY_ENDPOINT = '/api/nbbs/credibility/';
 export const SERVICES_ENDPOINT = '/api/nbbs/services/';
 export const VISITORS_ENDPOINT = '/api/nbbs/visitors/';
 export const BLOG_ENDPOINT = '/api/nbbs/blog/';
-export const CONTACT_US_ENDPOINT = '/api/nbbs/contact-us/';
+export const CONTACT_US_ENDPOINT = '/api/nbbs/contact-us';
 export const CAROUSELS_ENDPOINT = '/api/nbbs/carousels/';
 export const CASE_STUDIES_ENDPOINT = '/api/nbbs/case-studies/';
